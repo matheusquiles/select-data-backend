ae8d5627e9ec1f84c3463bf773b2ff29320d708c
Matheus Quiles <matheussquiles@gmail.com>
2024-10-09 17:05:33 -0300

    port

--- src/main/java/com/projeto/processos/ProcessosApplication.java
+++ src/main/java/com/projeto/processos/ProcessosApplication.java
@@ -1,5 +1,7 @@
 package com.projeto.processos;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProcessosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProcessosApplication.class, args);
+//		SpringApplication.run(ProcessosApplication.class, args);
+		
+		String port = System.getenv("PORT");
+	    if (port == null || port.isEmpty()) {
+	        port = "8080";
+	    }
+	    int portNumber = Integer.parseInt(port);
+	    // Configurar o servidor para usar a porta `portNumber`
+	    // Exemplo com Spring Boot
+	    SpringApplication app = new SpringApplication(ProcessosApplication.class);
+	    app.setDefaultProperties(Collections.singletonMap("server.port", portNumber));
+	    app.run(args);
 	}
 
 }
