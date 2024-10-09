2f2db9d27b9df1fc2992fe0398e7bba0e8df56bc
Matheus Quiles <matheussquiles@gmail.com>
2024-10-09 17:51:56 -0300

    configurações web

--- src/main/java/com/projeto/processos/config/SecurityConfig.java
+++ src/main/java/com/projeto/processos/config/SecurityConfig.java
@@ -2,14 +2,29 @@ package com.projeto.processos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig  {
 	
 	@Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+	
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors()  // Habilitar CORS
+            .and()
+            .csrf().disable()  // Desabilitar CSRF se necessário
+            .authorizeRequests()
+            .requestMatchers("/api/**").permitAll()  // Permitir acesso a todas as rotas da API
+            .anyRequest().authenticated();  // Outras requisições precisam de autenticação
+
+        return http.build();
     }
 
 
